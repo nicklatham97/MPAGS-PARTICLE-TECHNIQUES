7c343bfe655aeeb585b548ec2962bf26477a04ac
Nick Latham <phusjx@aspin.epp.warwick.ac.uk>
2021-01-21 18:53:47 +0000

    Initial commit

---
+++ CMakeFiles/exampleB5.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/exampleB5.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/exampleB5.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5ActionInitialization.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5ActionInitialization.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5CellParameterisation.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5CellParameterisation.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5DetectorConstruction.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5DetectorConstruction.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5DriftChamberHit.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5DriftChamberHit.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5DriftChamberSD.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5DriftChamberSD.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5EmCalorimeterHit.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5EmCalorimeterHit.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5EmCalorimeterSD.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5EmCalorimeterSD.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5EventAction.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5EventAction.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5HadCalorimeterHit.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5HadCalorimeterHit.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5HadCalorimeterSD.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5HadCalorimeterSD.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5HodoscopeHit.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5HodoscopeHit.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5HodoscopeSD.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5HodoscopeSD.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5MagneticField.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5MagneticField.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5PrimaryGeneratorAction.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5PrimaryGeneratorAction.cc.o"
+  "/home/physics/phusjx/EPPT2/MPAGS-PARTICLE-TECHNIQUES/src/B5RunAction.cc" "/home/physics/phusjx/EPPT2/CMakeFiles/exampleB5.dir/src/B5RunAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4GEOM_USE_USOLIDS"
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  "VECCORE_ENABLE_VC"
+  "VECGEOM_INPLACE_TRANSFORMATIONS"
+  "VECGEOM_NO_SPECIALIZATION"
+  "VECGEOM_QUADRILATERALS_VC"
+  "VECGEOM_REPLACE_USOLIDS"
+  "VECGEOM_ROOT"
+  "VECGEOM_USE_INDEXEDNAVSTATES"
+  "VECGEOM_USOLIDS"
+  "VECGEOM_VC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/cvmfs/sft.cern.ch/lcg/releases/clhep/2.4.1.0-2c56f/x86_64-centos7-gcc8-opt/lib/CLHEP-2.4.1.0/../../include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include/Geant4"
+  "/cvmfs/sft.cern.ch/lcg/releases/XercesC/3.1.3-ced0e/x86_64-centos7-gcc8-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/veccore/0.4.2-ff84f/x86_64-centos7-gcc8-opt/lib/cmake/VecCore/../../../include"
+  "/cvmfs/sft.cern.ch/lcg/releases/Vc/1.3.2-7fbe0/x86_64-centos7-gcc8-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.14.04-820c6/x86_64-centos7-gcc8-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/VecGeom/v00.05.01-34c21/x86_64-centos7-gcc8-opt/lib/cmake/VecGeom/../../../include"
+  "MPAGS-PARTICLE-TECHNIQUES/include"
+  "/cvmfs/sft.cern.ch/lcg/releases/clhep/2.4.1.0-2c56f/x86_64-centos7-gcc8-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include/QtWidgets"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include/QtGui"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include/QtCore"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/./mkspecs/linux-g++"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include/QtPrintSupport"
+  "/cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
